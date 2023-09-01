31566b4e2e5fdfdb6765dbbe1596c684496aa113
st5547312 <109869277+st5547312@users.noreply.github.com>
2023-09-01 08:16:08 +0800

    Delete rebot.bat

--- rebot.bat
+++
@@ -1,3 +0,0 @@
-reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v "NoViewContextMenu" /d 1 /t REG_DWORD /f
-
-start "" "C:\Users\Administrator\Desktop\yoeb-main\rebot.exe"
